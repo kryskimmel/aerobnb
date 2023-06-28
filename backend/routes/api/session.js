4c8c904484b13b43f0006ee33c2b622b75f1aeba
Krystal Kimmel <kabkimmel@gmail.com>
2023-06-28 18:06:06 -0500

    Modified logIn backend endpoint to return a user object with firstName and lastName keys

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -34,6 +34,8 @@ router.post(
 
       const safeUser = {
         id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
         email: user.email,
         username: user.username,
       };
