96012eb5844b60698e4e22d973c65f4f8e9d735c
Krystal Kimmel <kabkimmel@gmail.com>
2023-06-28 18:23:09 -0500

    Modified sign-up backend endpoint to include for firstName and lastName keys

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,9 +11,9 @@ const router = express.Router();
 router.post(
     '/',
     async (req, res) => {
-      const { email, password, username } = req.body;
+      const { email, password, username, firstName, lastName } = req.body;
       const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
+      const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
       const safeUser = {
         id: user.id,
