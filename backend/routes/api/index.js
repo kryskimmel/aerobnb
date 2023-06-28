efb4927641fb6dc6bfa18cb269433d187589634c
Krystal Kimmel <kabkimmel@gmail.com>
2023-06-28 16:05:04 -0500

    Add User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,49 +1,20 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const { restoreUser, requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { restoreUser } = require("../../utils/auth.js");
 
+// Connect restoreUser middleware to the API router
+  // If current user session is valid, set req.user to the user in the database
+  // If current user session is not valid, set req.user to null
 router.use(restoreUser);
-// router.use(requireAuth)
 
+router.use('/session', sessionRouter);
 
-/*
-router.post('/test', function(req,res){
-    res.json({requestBody: req.body})
-})
+router.use('/users', usersRouter);
 
-
-// GET /api/set-token-cookie
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-router.get('/set-token-cookie', async (_req, res) => {
-  const user = await User.findOne({
-    where: {
-      username: 'Demo-lition'
-    }
-  });
-  setTokenCookie(res, user);
-  return res.json({ user: user });
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
 });
-
-router.get(
-    '/restore-user',
-    (req, res) => {
-      return res.json(req.user);
-    }
-  );
-
-
-// GET /api/require-auth
-
-router.get(
-  '/require-auth',
-  requireAuth,
-  (req, res) => {
-    return res.json(req.user);
-  }
-);
-*/
-
-
 
 module.exports = router;
