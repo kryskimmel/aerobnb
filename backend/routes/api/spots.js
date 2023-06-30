d5afcf1fa8dcd582f7f97e3681e21542eb36661b
Krystal Kimmel <kabkimmel@gmail.com>
2023-06-30 10:20:54 -0500

    Updated get-all-spots api route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const { Op } = require('sequelize');
 const router = express.Router();
+const { Spot } = require('../../db/models');
 
+//get all spots
+router.get('/', async (req, res) => {
+    const getAllSpots = await Spot.findAll();
+
+    res.json(getAllSpots)
+})
 
 
 module.exports = router;
