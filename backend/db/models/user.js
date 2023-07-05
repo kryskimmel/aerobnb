0281cc446d8f4ae9ddd273a2e52e38fda4743962
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-05 13:41:00 -0500

    Modified User model association

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -6,7 +6,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Spot, {
-        foreignKey: 'ownerId'
+        foreignKey: 'ownerId',
+        onDelete:'CASCADE'
       })
     }
   };
