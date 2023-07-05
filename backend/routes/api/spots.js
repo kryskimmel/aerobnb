1f6878ea33e6522bfd997bf8880e02a2da45d21d
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-05 15:01:30 -0500

    modified spots api endpoints

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,61 +1,57 @@
 const express = require('express');
-const { Op } = require('sequelize');
-
+const { Sequelize, Op } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
-
-const { Spot } = require('../../db/models');
-const { User } = require('../../db/models');
-
+const { Spot, User } = require('../../db/models');
 const router = express.Router();
 
 
-const validateSpotCreation = [
-    check('address')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 5 })
-        .isString()
-        .withMessage('The address provided must be at least 5 characters long'),
-    check('city')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 2 })
-        .isString()
-        .withMessage('The city provided must be at least 2 characters long'),
-    check('state')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 2 })
-        .isString()
-        .withMessage('The state provided must be at least 2 characters long'),
-    check('country')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 2 })
-        .isString()
-        .withMessage('The country provided must be at least 2 characters long'),
-    check('lat')
-        .exists({ checkFalsy: true})
-        .isInt()
-        .withMessage('The input for latitude must be in number form'),
-    check('lng')
-        .exists({ checkFalsy: true})
-        .isInt()
-        .withMessage('The input for longitude must be in number form'),
-    check('name')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 2})
-        .isString()
-        .withMessage('The name provided must be at least 2 characters long'),
-    check('description')
-        .exists({ checkFalsy: true})
-        .isLength({ min: 2})
-        .isString()
-        .withMessage('The description provided must be at least 2 characters long'),
-    check('price')
-        .exists({ checkFalsy: true})
-        .isInt()
-        .withMessage('The input for price must be in number form'),
-    handleValidationErrors
-  ];
+// const validateSpotCreation = [
+//     check('address')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 5 })
+//         .isString()
+//         .withMessage('The address provided must be at least 5 characters long'),
+//     check('city')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 2 })
+//         .isString()
+//         .withMessage('The city provided must be at least 2 characters long'),
+//     check('state')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 2 })
+//         .isString()
+//         .withMessage('The state provided must be at least 2 characters long'),
+//     check('country')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 2 })
+//         .isString()
+//         .withMessage('The country provided must be at least 2 characters long'),
+//     check('lat')
+//         .exists({ checkFalsy: true})
+//         .isInt()
+//         .withMessage('The input for latitude must be in number form'),
+//     check('lng')
+//         .exists({ checkFalsy: true})
+//         .isInt()
+//         .withMessage('The input for longitude must be in number form'),
+//     check('name')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 2})
+//         .isString()
+//         .withMessage('The name provided must be at least 2 characters long'),
+//     check('description')
+//         .exists({ checkFalsy: true})
+//         .isLength({ min: 2})
+//         .isString()
+//         .withMessage('The description provided must be at least 2 characters long'),
+//     check('price')
+//         .exists({ checkFalsy: true})
+//         .isInt()
+//         .withMessage('The input for price must be in number form'),
+//     handleValidationErrors
+//   ];
 
 
 
@@ -64,14 +60,15 @@ const validateSpotCreation = [
 
 //get all spots
 router.get('/', async (req, res) => {
-    const getAllSpots = await Spot.findAll();
+    const getAllSpots = await Spot.findAll()
 
-    res.json({getAllSpots})
+    return res.json(getAllSpots)
 })
 
 
+
 //create a spot
-router.post('/', requireAuth, validateSpotCreation, async (req, res, next) => {
+router.post('/', requireAuth, async (req, res, next) => {
     const { address, city, state,
             country, lat, lng,
             name, description, price } = req.body;
