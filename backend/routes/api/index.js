66056a70d53ec8cdc7da5cecf248331fa482108c
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-12 13:35:41 -0500

    Added model, migration, and route handler for ReviewImages feature

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,9 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const spotsRouter = require('./spots.js')
+const spotsRouter = require('./spots.js');
+
+const reviewImagesRouter = require('./reviewImages.js')
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -13,7 +15,9 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
-router.use('/spots', spotsRouter)
+router.use('/spots', spotsRouter);
+
+router.use('review-images', reviewImagesRouter);
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
---
+++ backend/routes/api/reviewImages.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { Sequelize, Op, ValidationError } = require('sequelize');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
+const { ReviewImage } = require('../../db/models');
+const user = require('../../db/models/user');
+const router = express.Router();
+
+
+module.exports = router;
