a68c2673c9cc130619835f7ab0573fec369ade63
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-11 19:21:29 -0500

    Created endpoint to get all spots owned by the current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,75 +8,34 @@ const user = require('../../db/models/user');
 const router = express.Router();
 
 
-// const validateSpotCreation = [
-//     check('address')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 5 })
-//         .isString()
-//         .withMessage('The address provided must be at least 5 characters long'),
-//     check('city')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 2 })
-//         .isString()
-//         .withMessage('The city provided must be at least 2 characters long'),
-//     check('state')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 2 })
-//         .isString()
-//         .withMessage('The state provided must be at least 2 characters long'),
-//     check('country')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 2 })
-//         .isString()
-//         .withMessage('The country provided must be at least 2 characters long'),
-//     check('lat')
-//         .exists({ checkFalsy: true})
-//         .isInt()
-//         .withMessage('The input for latitude must be in number form'),
-//     check('lng')
-//         .exists({ checkFalsy: true})
-//         .isInt()
-//         .withMessage('The input for longitude must be in number form'),
-//     check('name')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 2})
-//         .isString()
-//         .withMessage('The name provided must be at least 2 characters long'),
-//     check('description')
-//         .exists({ checkFalsy: true})
-//         .isLength({ min: 2})
-//         .isString()
-//         .withMessage('The description provided must be at least 2 characters long'),
-//     check('price')
-//         .exists({ checkFalsy: true})
-//         .isInt()
-//         .withMessage('The input for price must be in number form'),
-//     handleValidationErrors
-//   ];
-
-
-
-
-
-
-//get all spots
+//Get all spots
 router.get('/', async (req, res) => {
     const getAllSpots = await Spot.findAll();
     return res.json(getAllSpots)
-})
+});
 
 
 
-//create a spot
+
+//Get all spots owned by the current user
+router.get('/current', requireAuth, async (req, res) => {
+    const ownerId = req.user.id;
+
+    const getSpotsByCurrUser = await Spot.findAll({
+        where: {ownerId}
+    })
+    return res.json(getSpotsByCurrUser)
+});
+
+
+
+//Create a spot
 router.post('/', requireAuth, handleValidationErrors, async (req, res, next) => {
     const { address, city, state,
             country, lat, lng,
             name, description, price } = req.body;
 
-
-    let ownerId = req.user.id;
-
-
+            const ownerId = req.user.id;
     try {
         const createSpot = await Spot.create({
             ownerId,
@@ -90,8 +49,7 @@ router.post('/', requireAuth, handleValidationErrors, async (req, res, next) => 
             description,
             price
         });
-
-        return res.status(201).json(createSpot)
+    return res.status(201).json(createSpot);
 
     } catch (error) {
         error.status = 400;
