2c7422e3e72c4f0f77c43c5959deb02b79746d56
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-15 13:28:13 -0500

    Created 403 error for when proper authorization is not met

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,6 +3,7 @@ const { Sequelize, Op, ValidationError } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
+const { isAuthorized } = require('../../utils/authorization')
 const { Spot, SpotImage, Review } = require('../../db/models');
 const router = express.Router();
 
@@ -106,7 +107,7 @@ router.post( '/', requireAuth, handleValidationErrors, async (req, res, next) =>
 
 /****************************************************** */
 //Add an Image to a Spot based on the Spot's id
-router.post( '/:spotId/images', requireAuth, async (req, res) => {
+router.post( '/:spotId/images', requireAuth, isAuthorized, async (req, res) => {
     const { url } = req.body;
 
     const findSpotbyId = await Spot.findByPk(req.params.spotId);
--- backend/utils/authorization.js
+++ backend/utils/authorization.js
@@ -1,9 +1,18 @@
-const { User } = require('../db/models');
+const { User, Spot } = require('../db/models');
+
+const isAuthorized = async function (req, res, next) {
+    const findSpotbyId = await Spot.findByPk(req.params.spotId)
+    if (req.user.id === findSpotbyId.ownerId) return next
+
+    const err = new Error('Forbidden');
+    // err.title = 'Authentication required';
+    err.errors = { message: 'Forbidden' };
+    err.status = 403;
+    return next(err);
+}
 
 
 
 
 
-
-
-// module.exports =
+module.exports = {isAuthorized}
