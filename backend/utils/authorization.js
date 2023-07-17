f98bb279407012e567f7198d24b8ab4c944fab25
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-16 23:56:48 -0500

    Updated isAuthorized middleware to function properly when continuing with route handler

--- backend/utils/authorization.js
+++ backend/utils/authorization.js
@@ -1,18 +1,18 @@
-const { User, Spot } = require('../db/models');
+const { Spot } = require('../db/models');
 
 const isAuthorized = async function (req, res, next) {
     const findSpotbyId = await Spot.findByPk(req.params.spotId)
-    if (req.user.id === findSpotbyId.ownerId) return next
-
-    const err = new Error('Forbidden');
-    // err.title = 'Authentication required';
-    err.errors = { message: 'Forbidden' };
-    err.status = 403;
-    return next(err);
-}
-
-
-
-
+    if (req.user.id === findSpotbyId.ownerId){
+        return next();
+    }
+    else {
+        const err = new Error('Forbidden');
+        err.message = 'Forbidden';
+        err.status = 403
+        delete err.stack;
+        err.title = 'Forbidden Request';
+        return next(err)
+    }
+};
 
 module.exports = {isAuthorized}
