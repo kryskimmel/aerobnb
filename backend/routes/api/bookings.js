84d7318e9492320762fa776b86256bad15c14b89
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-18 00:40:42 -0500

    Added code to get-all-of-current-users-bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -3,7 +3,7 @@ const { Sequelize, Op, ValidationError } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
-const { Booking } = require('../../db/models');
+const { Booking, Spot } = require('../../db/models');
 const router = express.Router();
 
 
@@ -13,7 +13,13 @@ router.get( '/current', requireAuth, async (req, res) => {
     const userId = req.user.id;
 
     const getBookingsByCurrUser = await Booking.findAll({
-        where: {userId}
+        where: {userId},
+        attributes: ['id', 'spotId'],
+        include: {
+            model: Spot,
+            attributes: {exclude: ['description', 'createdAt', 'updatedAt']},
+            include: 'previewImage'
+        },
     });
 
     return res.json(getBookingsByCurrUser)
