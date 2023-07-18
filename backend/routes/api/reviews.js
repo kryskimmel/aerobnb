71459f4ece9fd26ea3cfb01ebd9cf6413cb9e7f8
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-17 19:04:20 -0500

    Modified reviews endpoints file

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -1,18 +1,34 @@
 const express = require('express');
-const { Sequelize, Op, ValidationError } = require('sequelize');
+const { Sequelize, Op, ValidationError, where } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
-const { Review } = require('../../db/models');
+const { isAuthorized } = require('../../utils/authorization');
+const { notFound } = require('../../utils/notFound');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
 const router = express.Router();
 
+
+/****************************************************** */
+//Get all reviews of the current user
 router.get( '/current', requireAuth, async (req, res) => {
     const userId = req.user.id;
     const getReviewsByCurrUser = await Review.findAll({
         where: {userId}
     });
     return res.json(getReviewsByCurrUser)
-})
+});
 
+
+/****************************************************** */
+//Add an image to a review based on the review's id
+
+
+/****************************************************** */
+//Edit a review
+
+
+/****************************************************** */
+//Delete a review
 
 module.exports = router;
