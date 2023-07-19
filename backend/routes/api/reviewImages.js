afa8abd41dae3c20f3e12569013d54c66de9a3eb
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-18 19:53:22 -0500

    Completed Delete-image-for-a-review

--- backend/routes/api/reviewImages.js
+++ backend/routes/api/reviewImages.js
@@ -1,10 +1,41 @@
 const express = require('express');
-const { Sequelize, Op, ValidationError } = require('sequelize');
+const { Sequelize, Op, ValidationError, where } = require('sequelize');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
-const { ReviewImage } = require('../../db/models');
+const { isAuthorizedReview } = require('../../utils/isAuthorizedReview');
+const { reviewNotFound } = require('../../utils/reviewNotFound');
+const { Spot, SpotImage, Review, ReviewImage, User } = require('../../db/models');
 const router = express.Router();
 
+
+/****************************************************** */
+//Delete an existing image for a review
+router.delete ('/:imageId', requireAuth, async (req, res) => {
+    const findReviewImage = await ReviewImage.findByPk(req.params.imageId)
+    const reviewImageInfo = await ReviewImage.findOne({
+        where: {id: req.params.imageId},
+        include: {
+            model: Review
+        }
+    })
+
+     if (!reviewImageInfo) {
+        return res.status(404).json({message: 'Review Image couldn\'t be found'})
+    }
+
+    const reviewObj = reviewImageInfo.toJSON();
+    const {userId} = reviewObj.Review;
+    console.log(userId)
+
+    if (req.user.id !== userId) {
+        return res.status(403).json({message: 'Forbidden'})
+    }
+
+    else {
+        await findReviewImage.destroy()
+        return res.json({message: "Successfully deleted"})
+    };
+});
 
 module.exports = router;
