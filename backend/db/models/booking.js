9b143d93990949937ffcc32472010a2127033365
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-19 10:56:52 -0500

    Updated model files by adding onDelete Cascade

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -12,7 +12,9 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Booking.belongsTo(models.User, { foreignKey: 'userId' });
 
-      Booking.belongsTo(models.Spot, { foreignKey: 'spotId' });
+      Booking.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE' });
     }
   }
   Booking.init({
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -9,7 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
+      SpotImage.belongsTo(models.Spot, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE'
+      });
     }
   }
   SpotImage.init({
