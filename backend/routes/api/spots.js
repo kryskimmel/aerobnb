47ed01b611eee20238ec575e390ef663406e4770
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-19 00:50:56 -0500

    Updated validation handler object for Spots and completed Create-a-spot endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,6 +9,62 @@ const { Spot, SpotImage, Review, ReviewImage, User, Booking } = require('../../d
 const router = express.Router();
 
 
+const validateSignup = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .isString().withMessage('Please provide a street address that uses letters or alphanumeric characters')
+      .isLength({min: 5}).withMessage('Street address must have a minimum of 5 characters')
+      .isLength({max: 50}).withMessage('Street address must be less than 50 characters')
+      .notEmpty().withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .isString().withMessage('Please provide a city that uses alphabetic characters')
+        .isLength({min: 2}).withMessage('City must have a minimum of 2 characters')
+        .isLength({max: 20}).withMessage('City must be less than 20 characters')
+        .notEmpty().withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .isString().withMessage('Please provide a state that uses alphabetic characters')
+        .isLength({min: 2}).withMessage('State must have a minimum of 2 characters')
+        .isLength({max: 20}).withMessage('State must be less than 20 characters')
+        .notEmpty().withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .isString().withMessage('Please provide a country that uses alphabetic characters')
+        .isLength({min: 2}).withMessage('Country must have a minimum of 2 characters')
+        .isLength({max: 20}).withMessage('Country must be less than 20 characters')
+        .notEmpty().withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .not().isString().withMessage('Latitude is not valid')
+        .notEmpty().withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isDecimal()
+        .not().isString().withMessage('Longitude is not valid')
+        .notEmpty().withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isString().withMessage('Please provide a name that uses alphabetic characters')
+        .isLength({min: 2}).withMessage('Name must have a minimum of 2 characters')
+        .isLength({max: 50}).withMessage('Name must be less than 50 characters')
+        .notEmpty().withMessage('Name is required'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .isString().withMessage('Please provide a description that uses alphabetic characters')
+        .isLength({min: 2}).withMessage('Description must have a minimum of 2 characters')
+        .isLength({max: 500}).withMessage('Description must be less than 500 characters')
+        .notEmpty().withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .not().isString().withMessage('Price is not valid')
+        .notEmpty().withMessage('Price per day is required'),
+    handleValidationErrors
+  ];
+
+
 /****************************************************** */
 //Get all spots
 router.get( '/', async (req, res) => {
@@ -189,7 +245,7 @@ router.get( '/:spotId', spotNotFound, async (req, res, next) => {
 
 /****************************************************** */
 //Create a spot
-router.post( '/', requireAuth, handleValidationErrors, async (req, res, next) => {
+router.post( '/', requireAuth, validateSignup, async (req, res, next) => {
 
     try {
         const { address, city, state,
@@ -213,6 +269,7 @@ router.post( '/', requireAuth, handleValidationErrors, async (req, res, next) =>
 
     } catch (err) {
         err.status = 400
+        delete err.title
         next(err);
     }
 });
