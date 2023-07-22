372067b297c1ae332d4783e83d1b414e53d4c961
Krystal Kimmel <kabkimmel@gmail.com>
2023-07-21 20:31:08 -0500

    Added decimalNumbers to dialectOptions

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,6 +13,7 @@ module.exports = {
     dialect: 'postgres',
     seederStorage: 'sequelize',
     dialectOptions: {
+      "decimalNumbers": true,
       ssl: {
         require: true,
         rejectUnauthorized: false
