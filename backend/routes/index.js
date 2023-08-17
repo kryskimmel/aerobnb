c981fa67c042b659485d112a9d2d4e751d82c399
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-17 15:23:14 -0500

    Completed Authenticate Me Phase 0

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -16,6 +16,36 @@ router.get("/api/csrf/restore", (req, res) => {
 router.use('/api', apiRouter);
 
 
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+
+  router.use(express.static(path.resolve("../frontend/build")));
+
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+    );
+  });
+};
+
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
+  });
+};
+
 
 
 
