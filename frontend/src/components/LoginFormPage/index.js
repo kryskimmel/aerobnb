288e4000be8cde231b9a550c27b0b2766237339e
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-18 14:36:30 -0500

    Completed Phase 1 Authenticate Me

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,69 @@
+import React, {useState} from "react";
+import * as sessionActions from "../../store/session";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import "./LoginForm.css";
+
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+
+    if (sessionUser) return <Redirect to="/" />;
+
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setErrors({});
+      return dispatch(sessionActions.login({ credential, password })).catch(
+        async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+        }
+      );
+
+    };
+
+
+    return (
+        <div className="container-div">
+        <h1>Log In</h1>
+        <form onSubmit={handleSubmit} className="form-div">
+        <fieldset>
+        <div className="label-div">
+            <h3>Username or Email</h3>
+            <input
+                type="text"
+                name="username"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+                />
+
+        </div>
+        <div className="label-div">
+            <h3>Password</h3>
+            <input
+                type="password"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+        </div>
+        <div className="errors-div">
+            {errors.credential && <p>{errors.credential}</p>}
+        </div>
+        <div className="login-button-div">
+            <button type="submit">Log In</button>
+        </div>
+        </fieldset>
+        </form>
+        </div>
+    )
+}
+
+export default LoginFormPage;
---
+++ frontend/src/store/session.js
@@ -0,0 +1,87 @@
+// This file contains all actions specific to the session user's info and the session user's Redux reducer
+import { csrfFetch } from "./csrf";
+
+const SET_USER = 'session/SET_USER';
+const REMOVE_USER = 'session/REMOVE_USER';
+
+// Actions:
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    }
+};
+
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    }
+}
+
+// Thunk Action creators:
+export const login = (user) => async (dispatch) => {
+    const {credential, password} = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
+
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+// export const restoreSessionUser = (user) => async (dispatch) => {
+//     const {createdAt, email, id, updatedAt, username, firstName, lastName} = user;
+//     const response = await csrfFetch('/api/session', {
+//         method: 'GET',
+//         body: JSON.stringify({
+//             createdAt,
+//             email,
+//             id,
+//             updatedAt,
+//             username,
+//             firstName,
+//             lastName
+//         })
+//     });
+
+//     const data = await response.json();
+//     dispatch(setUser(data.user));
+//     return response
+// };
+
+
+
+
+
+
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        default:
+            return state;
+    }
+
+};
+
+
+export default sessionReducer;
