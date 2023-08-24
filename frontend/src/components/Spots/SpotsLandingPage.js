65356176f4b4f55105f3da02f33e5a694a24c052
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-24 17:45:33 -0500

    Added logic to Spots landing page

--- frontend/src/components/Spots/SpotsLandingPage.js
+++ frontend/src/components/Spots/SpotsLandingPage.js
@@ -15,20 +15,20 @@ const SpotsLandingPage = () => {
     }, [dispatch]);
 
 
-    // const spot =  allSpots.map(spot => (
-    //     <div key={spot.id} className="spots">
-    //         <img src={spot.previewImage} alt={spot.name}></img>
-    //     <div className="spot-info-div">
-    //     <div>
-    //         <p>{spot.city}, {spot.state}</p>
-    //             <p><span style={{fontWeight:"bold"}}>${spot.price}</span> night</p>
-    //     </div>
-    //     <div>
-    //         <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{spot.avgRating}</p>
-    //     </div>
-    //     </div>
-    //     </div>
-    // ))
+    const spot =  allSpots.map(spot => (
+        <div key={spot.id} className="spots">
+            <img src={spot.previewImage} alt={spot.name}></img>
+        <div className="spot-info-div">
+        <div>
+            <p>{spot.city}, {spot.state}</p>
+                <p><span style={{fontWeight:"bold"}}>${spot.price}</span> night</p>
+        </div>
+        <div>
+            <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{spot.avgRating}</p>
+        </div>
+        </div>
+        </div>
+    ))
 
 
 
@@ -36,7 +36,7 @@ const SpotsLandingPage = () => {
 
     return (
         <div className={allSpots.length > 0 ? 'all-spots-container' : 'no-spots-container'}>
-            {/* {spot} */}
+           {spot}
         </div>
 
     )
