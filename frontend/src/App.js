29e43bef60183db571c5aae91c93ef66e8e3f88f
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-23 19:29:52 -0500

    Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 // import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
-import LogInModal from "./components/Modals/LoginModal";
+import LoginModal from "./components/Modals/LoginModal";
+import SignupFormPage from "./components/SignupFormPage";
+import SpotsLandingPage from "./components/Spots/SpotsLandingPage";
+// import { LoginContext } from "./context/LoginModalContext";
+// import ('./components/Modals/LoginModal.css')
 
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
+
+
+  // const {openLogin} = useContext(LoginContext);
+
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
+
+  // const toggleOverlay = "add-overlay" + (openLogin ? "" : "no-overlay")
+  // const body = document.body.setAttribute("id", toggleOverlay)
 
   return (
     <>
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route exact path ="/">
+            <SpotsLandingPage/>
+          </Route>
           <Route path="/login">
-            <LogInModal />
+            <LoginModal/>
           </Route>
           <Route path="/signup">
             <SignupFormPage />
