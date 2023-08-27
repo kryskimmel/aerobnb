9a833ce2e6ab96fa5201490dec83a6e8956f33b0
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-27 15:17:02 -0500

    Added logic to spot detail component

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -1,28 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
 import * as spotActions from "../../store/spots";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import "./SpotDetail.css";
+import './css/SpotDetail.css';
 
-
-const SpotDetailPage = () => {
-    const {id} = useParams()
-
+const ShowDetail = () => {
     const dispatch = useDispatch();
+    const {id} = useParams();
+    const spot = useSelector(state => state.spots);
 
+     useEffect (() => {
+        dispatch(spotActions.fetchSingleSpot(id))
+     }, [dispatch, id])
 
-    const selectedSpot = useSelector(state => state.spots.currSpot);
-    const ownerInfo = useSelector(state => state.spots.currSpot.Owner);
-    const spotImages = useSelector(state => state.spots.currSpot.SpotImages);
-
-
-
-    useEffect(() => {
-        dispatch(spotActions.fetchSpotById(id))
-    }, [dispatch]);
-
-    const {
-        address,
+     const {
         avgStarRating,
         city,
         country,
@@ -30,44 +21,56 @@ const SpotDetailPage = () => {
         name,
         numReviews,
         price,
-        state
-    } = selectedSpot;
+        state,
+     } = spot;
 
 
-    const {
-        firstName,
-        lastName,
-    } = ownerInfo;
+    let previewImg;
+    let additionalImgs;
 
+     if (spot && spot.SpotImages) {
+        const spotArray = Object.values(spot.SpotImages);
 
-    const previewImg = spotImages.find(spot => spot.preview)
+        previewImg = spotArray.find(image => image.preview);
+        previewImg = previewImg.url
 
-    const filterAdditionalImgs = spotImages.filter((spot) => !spot.preview);
-    console.log('ADDITIONAL IMAGES', filterAdditionalImgs)
-
-    const additionalImgs = filterAdditionalImgs.map((image) => {
-        return (<img key={image.id} src={image.url}></img>)
-    })
-
-
-
+        const filterAdditionalImgs = spotArray.filter((image) => !image.preview);
+        additionalImgs = filterAdditionalImgs.map((image) => {
+            return (
+                <img src={image.url} alt={name}></img>
+            )
+       })
+     };
 
 
     return (
-        <div>
-            <h1>{name}</h1>
-            <h4>{city}, {state}, {country}</h4>
-            <img src={previewImg.url}></img>
-            {additionalImgs}
-
-
-            <h2>Hosted by {firstName} {lastName}</h2>
-            <h3><span>${price} night</span></h3>
-            <h3>{avgStarRating}</h3>
-            <h3>{numReviews} reviews</h3>
+        <div className='single-spot-container'>
+            <div className='single-spot-info-header'>
+                <h1>{name}</h1>
+                <h2>{city}, {state}, {country}</h2>
+            </div>
+            <div className='single-spot-imgs'>
+                <img className="preview-image" src={previewImg} alt={name}></img>
+                {additionalImgs}
+            </div>
+            <div className='single-spot-description'>
+                {spot && spot.Owner && (
+                    <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
+                )}
+                <p>{description}</p>
+            </div>
+            <div className='single-spot-info-box'>
+                <p><span>${price}</span> night</p>
+                <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i><span>{avgStarRating}</span></p>
+                <p>{numReviews} reviews</p>
+                <button onClick={() => {alert('Feature Coming Soon...')}}>Reserve</button>
+            </div>
+            <hr></hr>
+            <div className='single-spot-reviews'>
+                <p>Reviews here</p>
+            </div>
         </div>
     )
 };
 
-
-export default SpotDetailPage;
+export default ShowDetail;
