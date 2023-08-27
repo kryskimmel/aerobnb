6548287eaa3bc75157639f9cefbf39a56ff9192f
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-27 15:16:32 -0500

    Add spot detail route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useEffect} from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import * as spotActions from "./store/spots";
 import Navigation from "./components/Navigation";
 import LoginFormPage from "./components/LoginFormPage";
-
 import SignupFormPage from "./components/SignupFormPage";
-import SpotsLandingPage from "./components/Spots/SpotsLandingPage";
-import SpotDetailPage from "./components/Spots/SpotDetail";
-
 import ModalContainer from "./components/Modals/ModalContainer";
+import LandingPage from "./components/Spots/LandingPage";
+import ShowDetail from "./components/Spots/SpotDetail";
 
 
 
@@ -20,15 +18,10 @@ function App() {
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser())
-      .then(() => dispatch(spotActions.fetchAllSpots()))
-      .then(() => dispatch(spotActions.fetchSpotById(1)))
+      .then(() => dispatch(spotActions.fetchSpots()))
       .then(() => {setIsLoaded(true)})
-
-      return () => {
-
-      }
-
   }, [dispatch]);
+
 
 
 
@@ -38,17 +31,17 @@ function App() {
       <ModalContainer/>
       {isLoaded && (
         <Switch>
-          <Route exact path ="/">
-            <SpotsLandingPage/>
+          <Route exact path="/">
+            <LandingPage/>
+          </Route>
+          <Route path="/spots/:id">
+            <ShowDetail/>
           </Route>
           <Route path="/login">
             <LoginFormPage/>
           </Route>
           <Route path="/signup">
             <SignupFormPage/>
-          </Route>
-          <Route path ="/spots/:id">
-            <SpotDetailPage/>
           </Route>
         </Switch>
       )}
