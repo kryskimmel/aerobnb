5f6de11542af96097ba842a3b8ea38e7fd5ee3aa
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-27 15:27:33 -0500

    Created form component to create a spot

---
+++ frontend/src/components/Spots/CreateSpot.js
@@ -0,0 +1,16 @@
+import React, {createElement, useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {useHistory} from 'react-router-dom';
+import * as spotActions from "../../store/spots";
+
+const CreateSpot = () => {
+
+
+    return (
+        <>
+        </>
+    )
+};
+
+
+export default CreateSpot;
