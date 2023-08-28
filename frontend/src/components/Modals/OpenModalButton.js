96879d9a94364ba1f1b27bcd3544b33e06d5f3fa
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-27 22:47:49 -0500

    Created open modal button component

---
+++ frontend/src/components/Modals/OpenModalButton.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import useModal from '../../context/OpenModalContext';
+
+function OpenModalButton({
+  modalComponent, // component to render inside the modal
+  buttonText, // text of the menu item that opens the modal
+  onButtonClick, // optional: callback function that will be called once the menu item that opens the modal is clicked
+  onModalClose // optional: callback function that will be called once the modal is closed
+}) {
+  const { onModalContent, setModalContent, setOnModalClose } = useModal();
+
+  const onClick = () => {
+    if (onModalClose) setOnModalClose(onModalClose);
+    setModalContent(modalComponent);
+    if (onButtonClick) onButtonClick();
+  };
+
+  console.log(onModalContent, ':modal content')
+
+  return (
+    <button onClick={onClick}>{buttonText}</button>
+  );
+}
+
+export default OpenModalButton;
