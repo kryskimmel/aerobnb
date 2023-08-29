295c7e1bde45ea296576943923f168c6bf203e4a
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-28 19:54:29 -0500

    Added divs to modal for styling

--- frontend/src/components/Modals/DeleteSpotModal.js
+++ frontend/src/components/Modals/DeleteSpotModal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import useModal from "../../context/OpenModalContext";
+import './DeleteSpotModal.css'
 
 
 const DeleteSpotModal = () => {
@@ -10,12 +11,15 @@ const DeleteSpotModal = () => {
     const close = history.push('/spots/current');
 
     return (
-        <div>
-            <h1>Confirm Delete</h1>
-            <h4>Are you sure you want to remove this spot from the listings?</h4>
-            <button>Yes (Delete Spot)</button>
-            <button onClick={closeModal}>No (Keep Spot)</button>
+        <div className="overlay">
+            <div className="delete-modal">
+                <h1>Confirm Delete</h1>
+                <h4>Are you sure you want to remove this spot from the listings?</h4>
+                <button>Yes (Delete Spot)</button>
+                <button onClick={closeModal}>No (Keep Spot)</button>
+            </div>
         </div>
+
 
     )
 };
