e0b7b449eeb421970ebc239d9f53abc14e18f9aa
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-29 18:18:30 -0500

    Fixed logic so that it will show only owner-owned spots

--- frontend/src/components/Spots/SpotManagement.js
+++ frontend/src/components/Spots/SpotManagement.js
@@ -3,30 +3,22 @@ import {useDispatch, useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import { nanoid } from 'nanoid';
 import * as spotActions from "../../store/spots";
+import * as sessionActions from "../../store/session";
 import useModal from '../../context/OpenModalContext';
 import OpenModalButton from '../Modals/OpenModalButton';
 import DeleteSpotModal from '../Modals/DeleteSpotModal';
 import './css/SpotManagement.css';
 
 
-// const SpotManagement = () => {
-//     return (
-//         <>
-//         <div className='header-div'>
-//             <h1>Manage Your Spots</h1>
-//             <button className='create-spot-button'>Create a New Spot</button>
-//         </div>
-//         <LandingPage/>
-//         </>
-//     )
-// };
-
-
 const SpotManagement = () => {
     const dispatch = useDispatch();
     const history = useHistory();
+    const currUser = useSelector((state) => state.session.user);
     const spots = useSelector(state => Object.values(state.spots));
     const [spotId, setSpotId] = useState();
+
+    console.log('CURR USER:', currUser)
+    console.log('ALL SPOTS:', spots)
 
     useEffect(() => {
         dispatch(spotActions.fetchSpots());
@@ -42,6 +34,7 @@ const SpotManagement = () => {
     const allSpots = spots.map((spot) => {
         const {
             id,
+            ownerId,
             name,
             city,
             state,
@@ -50,30 +43,31 @@ const SpotManagement = () => {
             avgRating
         } = spot
 
+        if (currUser.id ===ownerId) {
+            return (
+                <div key={spot.id} className='spot-card'>
+                    <img src={previewImage} alt={name} onClick={() => {history.push(`/spots/${id}`)}}></img>
+                    <div key={spot.id} className='spot-info'>
+                        <p className='location-info'>{city}, {state}</p>
+                        <p className='rating-info'><i className="fa-solid fa-star" style={{color: "#000000"}}></i><span> {avgRating}</span></p>
+                        <p className='price-info'><span>${price}</span> night</p>
+                    </div>
+                    <div className="show-mgmt" id={`mgmt-${spot.id}`} onClick={(prev) => prev = setSpotId(spot.id)}>
 
-        return (
-            <div key={spot.id} className='spot-card'>
-                <img src={previewImage} alt={name} onClick={() => {history.push(`/spots/${id}`)}}></img>
-                <div key={spot.id} className='spot-info'>
-                    <p className='location-info'>{city}, {state}</p>
-                    <p className='rating-info'><i className="fa-solid fa-star" style={{color: "#000000"}}></i><span> {avgRating}</span></p>
-                    <p className='price-info'><span>${price}</span> night</p>
+                        <OpenModalButton
+                            buttonText="Update"
+                            onButtonClick
+                            modalComponent
+                        />
+                        <OpenModalButton
+                            buttonText="Delete"
+                            onButtonClick={handleModalOpen}
+                            modalComponent={<DeleteSpotModal/>}
+                        />
+                    </div>
                 </div>
-                <div className="show-mgmt" id={`mgmt-${spot.id}`} onClick={(prev) => prev = setSpotId(spot.id)}>
-
-                    <OpenModalButton
-                        buttonText="Update"
-                        onButtonClick
-                        modalComponent
-                    />
-                    <OpenModalButton
-                        buttonText="Delete"
-                        onButtonClick={handleModalOpen}
-                        modalComponent={<DeleteSpotModal/>}
-                    />
-                </div>
-            </div>
-        )
+            )
+        }
     })
 
 
