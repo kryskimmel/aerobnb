c4543b2e6befdec2716b5d7f780a83894a12e643
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-31 18:21:04 -0500

    Dispatched deleteReview action

--- frontend/src/components/Modals/DeleteReviewModal.js
+++ frontend/src/components/Modals/DeleteReviewModal.js
@@ -5,24 +5,21 @@ import useModal from "../../context/OpenModalContext";
 import './DeleteReviewModal.css'
 
 
-const DeleteReviewModal = ({spotId}) => {
+const DeleteReviewModal = ({reviewId}) => {
     const dispatch = useDispatch();
     const {closeModal} = useModal();
 
-
-
-
-    // const handleYes = () => {
-    //     closeModal();
-    //     dispatch(spotActions.deleteSingleSpot(spotId));
-    // };
+    const handleYes = () => {
+        closeModal();
+        dispatch(reviewActions.deleteSingleReview(reviewId));
+    };
 
     return (
         <div className="overlay">
             <div className="delete-modal">
                 <h1>Confirm Delete</h1>
                 <h4>Are you sure you want to delete this review?</h4>
-                <button className="yes-button">Yes (Delete Review)</button>
+                <button className="yes-button" onClick={handleYes}>Yes (Delete Review)</button>
                 <button className="no-button" onClick={closeModal}>No (Keep Review)</button>
             </div>
         </div>
