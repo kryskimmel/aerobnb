c70f397c2712e9e352cfe084fd112a3d60a13b70
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-31 17:50:49 -0500

    Added DeleteReviewModal file

---
+++ frontend/src/components/Modals/DeleteReviewModal.js
@@ -0,0 +1,34 @@
+import React, {useEffect} from "react";
+import { useDispatch } from "react-redux";
+import * as reviewActions from "../../store/reviews";
+import useModal from "../../context/OpenModalContext";
+import './DeleteReviewModal.css'
+
+
+const DeleteReviewModal = ({spotId}) => {
+    const dispatch = useDispatch();
+    const {closeModal} = useModal();
+
+
+
+
+    // const handleYes = () => {
+    //     closeModal();
+    //     dispatch(spotActions.deleteSingleSpot(spotId));
+    // };
+
+    return (
+        <div className="overlay">
+            <div className="delete-modal">
+                <h1>Confirm Delete</h1>
+                <h4>Are you sure you want to delete this review?</h4>
+                <button className="yes-button">Yes (Delete Review)</button>
+                <button className="no-button" onClick={closeModal}>No (Keep Review)</button>
+            </div>
+        </div>
+
+
+    )
+};
+
+export default DeleteReviewModal;
