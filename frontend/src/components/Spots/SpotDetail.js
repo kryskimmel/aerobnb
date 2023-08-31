7818f1567ebb44d2698321e420cfca924a6cfce9
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-31 17:24:02 -0500

    Modified file to include logic for handling reviews

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -2,18 +2,23 @@ import React, {useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import * as spotActions from "../../store/spots";
+import * as reviewActions from "../../store/reviews";
+import { monthEquivalent } from '../../utilities/monthEquivalencies';
 import './css/SpotDetail.css';
 
 const ShowDetail = () => {
     const dispatch = useDispatch();
     const {id} = useParams();
     const spot = useSelector(state => state.spots);
+    const reviews = useSelector(state => state.reviews);
+    const sessionUser = useSelector(state => state.session.user);
 
      useEffect (() => {
         dispatch(spotActions.fetchSingleSpot(id))
+        dispatch(reviewActions.fetchSpotReviews(id))
      }, [dispatch, id])
 
-     const {
+     const{
         avgStarRating,
         city,
         country,
@@ -24,9 +29,14 @@ const ShowDetail = () => {
         state,
      } = spot;
 
-
     let previewImg;
     let additionalImgs;
+
+    const reviewsText = (numReviews) => {
+        if (numReviews === 0) return 'New';
+        if (numReviews === 1) return 'Review';
+        if (numReviews > 1) return 'Reviews';
+    }
 
      if (spot && spot.SpotImages) {
         const spotArray = Object.values(spot.SpotImages);
@@ -37,10 +47,38 @@ const ShowDetail = () => {
         const filterAdditionalImgs = spotArray.filter((image) => !image.preview);
         additionalImgs = filterAdditionalImgs.map((image) => {
             return (
-                <img src={image.url} alt={name}></img>
+                <img src={image.url} alt={name} className='additional-imgs'></img>
             )
        })
      };
+
+
+
+    let reviewText;
+
+    if (reviews) {
+        const reviewsArray = Object.values(reviews);
+        reviewText = reviewsArray.map((eachReview) => {
+            if (eachReview && eachReview.User && sessionUser) {
+                const year = eachReview.createdAt.slice(0, 4);
+                const month = eachReview.createdAt.slice(5,7);
+
+                const deleteButtonClassName = sessionUser.id === eachReview.userId ? "show-delete" : "hide-delete";
+                const updateButtonClassName = sessionUser.id === eachReview.userId ? "show-update" : "hide-update";
+
+                return (
+                    <div className='each-review-div'>
+                        <h3>{eachReview.User.firstName}</h3>
+                        <h4 style={{color:"#999999", fontWeight:"500"}}>{monthEquivalent(month)} {year}</h4>
+                        <p>{eachReview.review}</p>
+                        <button className={updateButtonClassName}>Update</button>
+                        <button className={deleteButtonClassName}>Delete</button>
+                    </div>
+                )
+            }
+
+        })
+    };
 
 
     return (
@@ -53,21 +91,23 @@ const ShowDetail = () => {
                 <img className="preview-image" src={previewImg} alt={name}></img>
                 {additionalImgs}
             </div>
-            <div className='single-spot-description'>
-                {spot && spot.Owner && (
-                    <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
-                )}
-                <p>{description}</p>
-            </div>
-            <div className='single-spot-info-box'>
-                <p><span>${price}</span> night</p>
-                <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i><span>{avgStarRating}</span></p>
-                <p>{numReviews} reviews</p>
-                <button onClick={() => {alert('Feature Coming Soon...')}}>Reserve</button>
+            <div className='single-spot-info-section'>
+                <div className='single-spot-description'>
+                    {spot && spot.Owner && (
+                        <h2>Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
+                    )}
+                    <p>{description}</p>
+                </div>
+                <div className='single-spot-info-box'>
+                    <p style={{textAlign:"left", padding:"0 25px"}}><span style={{fontSize:"20px"}}>${price}</span> night</p>
+                    <p style={{textAlign:"right", padding:"0 25px"}}><i className="fa-solid fa-star" style={{color: "#000000"}}></i><span>{avgStarRating}</span> {numReviews ? `• ${numReviews} `: ""} {reviewsText(numReviews)}</p>
+                    <button className='reserve-button' onClick={() => {alert('Feature Coming Soon...')}}>Reserve</button>
+                </div>
             </div>
             <hr></hr>
             <div className='single-spot-reviews'>
-                <p>Reviews here</p>
+                <h2><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{`${avgStarRating ? avgStarRating : ""} ${numReviews ? `• ${numReviews} ` : ""} ${reviewsText(numReviews)}`}</h2>
+                {reviewText}
             </div>
         </div>
     )
