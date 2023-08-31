ab3dc921749239993ac4dc6f6bda18922975c795
Krystal Kimmel <kabkimmel@gmail.com>
2023-08-31 17:21:42 -0500

    Added store file to handle reviews

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,74 @@
+import { csrfFetch } from "./csrf";
+
+
+const LOAD = "reviews/LOAD";
+const CREATE = "reviews/CREATE";
+const DELETE = "reviews/DELETE";
+
+
+//Actions:
+const loadReviews = (reviews) => {
+    return {
+        type: LOAD,
+        payload: reviews
+    }
+};
+
+
+const createReview = (review) => {
+    return {
+        type: CREATE,
+        payload: review
+    }
+};
+
+
+const deleteReview = (review) => {
+    return {
+        type: DELETE,
+        payload: review
+    }
+};
+
+
+
+//Thunk Action Creators:
+
+export const fetchSpotReviews = (spotId) => async (dispatch) => {
+    try {
+        const response = await csrfFetch(`/api/spots/${spotId}/reviews`, {
+            method: 'GET'
+        })
+
+        if (response.ok) {
+            const reviews = await response.json();
+            dispatch(loadReviews(reviews));
+            return response;
+        }
+        else throw new Error(`Failed to fetch all reviews for the spot with an id of ${spotId}`);
+
+    }
+    catch (error) {
+        throw new Error(`The following error has occurred while fetching the reviews for the selected spot: ${error.message}`)
+    }
+};
+
+
+//Reducer:
+const initialState = {reviews: null};
+
+const reviewReducer = (state = initialState, action) => {
+    let newState = {};
+
+    switch (action.type) {
+        case LOAD:
+            if (action.payload.Reviews) {
+                action.payload.Reviews.forEach((review) => newState[review.id] = review);
+                return newState;
+            }
+        default:
+            return state;
+    }
+}
+
+export default reviewReducer;
