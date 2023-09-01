d08fc6f01bd6a1df98ccf7c7f6f5e825fb908037
Krystal Kimmel <kabkimmel@gmail.com>
2023-09-01 00:31:42 -0500

    Copied logic from SignUpForm and altered it

--- frontend/src/components/Modals/SignupFormModal.js
+++ frontend/src/components/Modals/SignupFormModal.js
@@ -1,28 +1,117 @@
-import React, {useContext} from "react";
-import { useSelector } from "react-redux";
-import { OpenModalMenuContext } from "../../context/OpenModalContext";
+import React, {useState, useEffect} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
+import * as sessionActions from "../../store/session";
 import "./css/SignupFormModal.css";
-import SignupFormPage from "../SignupFormPage";
+
 
 const SignupFormModal = () => {
 
-    const currUser = useSelector(state => state.session.user);
-    console.log('THE CURRENT USER:', currUser)
+    const dispatch = useDispatch();
+    const sessionUser = useSelector((state) => state.session.user);
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState({});
 
+    if (sessionUser) return <Redirect to="/" />;
 
-    const modalContainerClassName = currUser ? "hidden" : "show";
-    const toggleOverlay = currUser ? null : "overlay";
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            setErrors({});
+            return dispatch(sessionActions.signup({
+                email,
+                username,
+                firstName,
+                lastName,
+                password,
+            })).catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) {
+                    setErrors(data.errors);
+                }
+            });
+        }
+    return setErrors({
+      confirmPassword: "Confirm Password field must be the same as the Password field"
+    });
+  };
+
+    const buttonClassName = "enabled-button" + (email && username && firstName && lastName && password && confirmPassword ? "" : " disabled-button");
 
 
     return (
-        <div className={modalContainerClassName} id={toggleOverlay}>
-            <div className={modalContainerClassName} id="signup-modal">
-              <SignupFormPage/>
+        <div className="overlay">
+            <div className="signup-modal">
+                <div className="form-container-div">
+                    <h1>Sign Up</h1>
+                    <div className="errors-div">
+                        {errors.firstName && <p>{errors.firstName}</p>}
+                        {errors.lastName && <p>{errors.lastName}</p>}
+                        {errors.email && <p>{errors.email}</p>}
+                        {errors.username && <p>{errors.username}</p>}
+                        {errors.password && <p>{errors.password}</p>}
+                        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+                    </div>
+                    <form onSubmit={handleSubmit} className="form-div">
+                        <div className="field-group" id="field-group-1">
+                            <label className="label">First Name</label>
+                                <input
+                                type="text"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                                required
+                                />
+                            <label className="label">Last Name</label>
+                                <input
+                                type="text"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                                required
+                                />
+                        </div>
+                        <div className="field-group" id="field-group-2">
+                            <label className="label">Email</label>
+                                <input
+                                type="text"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                                />
+                            <label className="label">Username</label>
+                                <input
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                />
+                        </div>
+                        <div className="field-group" id="field-group-3">
+                            <label className="label">Password</label>
+                                <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                                />
+                            <label className="label">Confirm Password</label>
+                                <input
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                required
+                                />
+                        </div>
+                        <button type="submit" className={buttonClassName}>Sign Up</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
-
-
-}
+};
 
 export default SignupFormModal;
