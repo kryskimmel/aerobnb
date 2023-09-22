25b0d2f0935cb519d2793840e1d408f511d21c8f
Krystal Kimmel <kabkimmel@gmail.com>
2023-09-21 22:33:22 -0500

    Modified classnames;

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -35,30 +35,32 @@ function SpotDetail() {
     })
 
     return (
-        <div className="single-spot-container">
-            <div className="single-spot-info-header">
+        <div className="spot-container">
+            <div className="spot-header">
                 <h1>{currSpot.name}</h1>
                 <h2>{currSpot.city}, {currSpot.state}</h2>
             </div>
 
             <div className="imgs-div">
                 <div className="preview-img-div">
-                    <img src={previewImg && previewImg.url} alt={currSpot.name} className="preview-image"></img>
+                    <img src={previewImg && previewImg.url} alt={currSpot.name} className="preview-img"></img>
                 </div>
                 <div className="additional-imgs-div">
                     {additionalImgs}
                 </div>
             </div>
 
-            <div className="single-spot-info-container">
-                <div className="single-spot-description">
+            <div className="spot-info-container">
+                <div className="spot-description">
                     <h2>{`Hosted by ${currSpot.Owner?currSpot.Owner.firstName:""} ${currSpot.Owner?currSpot.Owner.lastName:""}`}</h2>
                     <p>{currSpot.description}</p>
                 </div>
-                <div className="single-spot-info">
-                    <p>${currSpot.price} night</p>
-                    <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{currSpot.avgStarRating ? currSpot.avgStarRating : "New"} • {currSpot.numReviews && currSpot.numReviews === 1 ? `${currSpot.numReviews} review` : `${currSpot.numReviews} reviews`}</p>
-                    <button onClick={() => {alert("Feature Coming Soon...")}}>Reserve</button>
+                <div className="spot-info-box">
+                    <ul>
+                        <li>${currSpot.price} night</li>
+                        <li><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{currSpot.avgStarRating ? currSpot.avgStarRating : "New"} • {currSpot.numReviews && currSpot.numReviews === 1 ? `${currSpot.numReviews} review` : `${currSpot.numReviews} reviews`}</li>
+                    </ul>
+                    <button className="reserve-button" onClick={() => {alert("Feature Coming Soon...")}}>Reserve</button>
                 </div>
             </div>
 
@@ -81,10 +83,10 @@ function SpotDetail() {
             {sessionUser && currSpotReviews && currSpotReviews.map((review) => {
                 if (review && review.User) {
                     return (
-                        <div className="review">
+                        <div className="reviews">
                             <ul>
-                                <li>{review.User?review.User.firstName:""}</li>
-                                <li>{monthEquivalent(review.createdAt.slice(5,7))} {review.createdAt.slice(0,4)}</li>
+                                <li style={{fontWeight:"bold"}}>{review.User?review.User.firstName:""}</li>
+                                <li style={{color:"#989898"}}>{monthEquivalent(review.createdAt.slice(5,7))} {review.createdAt.slice(0,4)}</li>
                                 <li>{review.review}</li>
                             </ul>
                             <div className={review.userId === sessionUser.id ? "show-button" : "hide-buttons"}>
