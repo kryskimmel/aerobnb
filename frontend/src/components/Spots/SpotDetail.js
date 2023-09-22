1dcdfae79a60be7c559ec8f8e73880d214eadf91
Krystal Kimmel <kabkimmel@gmail.com>
2023-09-21 21:41:11 -0500

    Added optional chaining

--- frontend/src/components/Spots/SpotDetail.js
+++ frontend/src/components/Spots/SpotDetail.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import * as spotActions from "../../store/spots";
@@ -19,10 +19,11 @@ function SpotDetail() {
     const {id} = useParams();
     const {setOnModalContent} = useModal();
 
+
     useEffect(() => {
         dispatch(spotActions.fetchSingleSpot(id));
         dispatch(reviewActions.fetchSpotReviews(id))
-    }, [id])
+    }, [dispatch, id])
 
 
     const previewImg = currSpot && currSpot.SpotImages && currSpot.SpotImages.find(image => image.preview === true)
@@ -33,54 +34,61 @@ function SpotDetail() {
         )
     })
 
-    const loadSpotDetails = currSpot && currSpot.Owner && currSpotReviews && sessionUser && (
-        <>
-        <div className="single-spot-info-header">
-            <h1>{currSpot.name}</h1>
-            <h2>{currSpot.city}, {currSpot.state}</h2>
-        </div>
-        <div className="imgs-div">
-            <div className="preview-img-div">
-            <img src={previewImg && previewImg.url} alt={currSpot.name} className="preview-image"></img>
+    return (
+        <div className="single-spot-container">
+            <div className="single-spot-info-header">
+                <h1>{currSpot.name}</h1>
+                <h2>{currSpot.city}, {currSpot.state}</h2>
             </div>
-            <div className="additional-imgs-div">
-                {additionalImgs}
-            </div>
-        </div>
-        <div className="single-spot-info-container">
-            <div className="single-spot-description">
-                <h2>{`Hosted by ${currSpot.Owner.firstName} ${currSpot.Owner.lastName}`}</h2>
-                <p>{currSpot.description}</p>
-            </div>
-            <div className="single-spot-info">
-                <p>${currSpot.price} night</p>
-                <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{currSpot.avgStarRating ? currSpot.avgStarRating : "New"} • {currSpot.numReviews && currSpot.numReviews === 1 ? `${currSpot.numReviews} review` : `${currSpot.numReviews} reviews`}</p>
-                <button onClick={() => {alert("Feature Coming Soon...")}}>Reserve</button>
-            </div>
-        </div>
 
-        <hr></hr>
-        <div className="reviews-container">
-            <div className="review-info">
+            <div className="imgs-div">
+                <div className="preview-img-div">
+                    <img src={previewImg && previewImg.url} alt={currSpot.name} className="preview-image"></img>
+                </div>
+                <div className="additional-imgs-div">
+                    {additionalImgs}
+                </div>
+            </div>
+
+            <div className="single-spot-info-container">
+                <div className="single-spot-description">
+                    <h2>{`Hosted by ${currSpot.Owner?currSpot.Owner.firstName:""} ${currSpot.Owner?currSpot.Owner.lastName:""}`}</h2>
+                    <p>{currSpot.description}</p>
+                </div>
+                <div className="single-spot-info">
+                    <p>${currSpot.price} night</p>
+                    <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{currSpot.avgStarRating ? currSpot.avgStarRating : "New"} • {currSpot.numReviews && currSpot.numReviews === 1 ? `${currSpot.numReviews} review` : `${currSpot.numReviews} reviews`}</p>
+                    <button onClick={() => {alert("Feature Coming Soon...")}}>Reserve</button>
+                </div>
+            </div>
+
+            <hr></hr>
+
+            <div className="reviews-header">
                 <p><i className="fa-solid fa-star" style={{color: "#000000"}}></i>{currSpot.avgStarRating ? currSpot.avgStarRating : "New"} • {currSpot.numReviews && currSpot.numReviews === 1 ? `${currSpot.numReviews} review` : `${currSpot.numReviews} reviews`}</p>
             </div>
-            <div className="reviews-div">
-                <div className={currSpot.Owner.id !== sessionUser.id && !currSpotReviews.length || currSpotReviews.find((rev => rev.userId !== sessionUser.id)) ? "show-button" : "hide-buttons"}>
-                    {console.log(currSpot.Owner.id,currSpotReviews )}
+
+            {currSpot.Owner && sessionUser && (
+                <div className={currSpot.Owner.id !== sessionUser.id ? "show-button" : "hide-buttons"}>
                     <OpenModalButton
                         buttonText="Post Your Review"
-                        onButtonClick={() => {setOnModalContent(<PostReviewModal spotId={id}/>)}}
+                        onButtonClick={() => {setOnModalContent(<PostReviewModal spotId={id} />)}}
                         modalComponent={<PostReviewModal />}
                     />
                 </div>
-                {currSpotReviews && currSpotReviews.map((review) => {
+            )}
+
+            {sessionUser && currSpotReviews && currSpotReviews.map((review) => {
+                if (review && review.User) {
                     return (
                         <div className="review">
-                            <p>{ review.User && review.User.firstName}</p>
-                            <p>{monthEquivalent(review.createdAt.slice(5,7))} {review.createdAt.slice(0,4)}</p>
-                            <p>{review.review}</p>
+                            <ul>
+                                <li>{review.User?review.User.firstName:""}</li>
+                                <li>{monthEquivalent(review.createdAt.slice(5,7))} {review.createdAt.slice(0,4)}</li>
+                                <li>{review.review}</li>
+                            </ul>
                             <div className={review.userId === sessionUser.id ? "show-button" : "hide-buttons"}>
-                                <OpenModalButton
+                            <OpenModalButton
                                     buttonText="Delete"
                                     onButtonClick={() => {setOnModalContent(<DeleteReviewModal reviewId={review.id} spotId={id}/>)}}
                                     modalComponent={<DeleteReviewModal />}
@@ -88,17 +96,9 @@ function SpotDetail() {
                             </div>
                         </div>
                     )
-                })}
-            </div>
-        </div>
+                }
 
-        </>
-    )
-
-
-    return (
-        <div className="single-spot-container">
-            {loadSpotDetails}
+            })}
         </div>
     )
 }
